7e779ac25e452cd0746dc60d43da67ee2b931e37
neal <46561493+neal2018@users.noreply.github.com>
2020-09-06 23:26:03 +0800

    Update CardMap version

--- src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/GwentGame/GwentMap.cs
@@ -189,7 +189,7 @@ namespace Cynthia.Card
         }
 
         //更新CardMap内容请务必将CardMapVersion更新
-        public static Version CardMapVersion { get; } = new Version(1, 0, 0, 62);
+        public static Version CardMapVersion { get; } = new Version(1, 0, 0, 63);
 
 
         public class MultilingualString
