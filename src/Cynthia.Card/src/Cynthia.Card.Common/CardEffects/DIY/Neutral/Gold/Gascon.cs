e93b136c35f0e20d8eae7789ad40f2221c694de8
neal <46561493+neal2018@users.noreply.github.com>
2020-09-17 16:57:30 +0800

    让加斯科能存活

--- src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Gold/Gascon.cs
+++ src/Cynthia.Card/src/Cynthia.Card.Common/CardEffects/DIY/Neutral/Gold/Gascon.cs
@@ -8,42 +8,11 @@ namespace Cynthia.Card
     [CardEffectId("70032")]//Gascon
     public class Gascon : CardEffect, IHandlesEvent<AfterCardMove>
     {//将所有单位移至随机排。每移动1个单位，便受到3点伤害。若位于牌组或手牌：己方回合中，每有1个单位被改变所在排别时获得1点增益。
-/*      public Gascon(GameCard card) : base(card) { }
-        public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
-        {
-            var cards = Game.GetAllCard(Card.PlayerIndex, isHasConceal: true).Where(x => x.Status.CardRow.IsOnPlace() && x != Card).ToList();
-            foreach (var card in cards)
-            {
-                var row = (card.Status.CardRow.MyRowToIndex()).IndexToMyRow();
-                var targetRow = TurnType.My.GetRow();
-                targetRow.Remove(row.IsMyRow() ? row : row.Mirror());
-                var canMoveRow = targetRow.Where(x => Game.RowToList(card.PlayerIndex, x).Count < Game.RowMaxCount);
-                if (!canMoveRow.TryMessOne(out var target, Game.RNG))
-                {
-                    continue;
-                }
-                await card.Effect.Move(new CardLocation(target, Game.RowToList(card.PlayerIndex, target).Count), Card);
-            }
-            var listCount = cards.Count();
-            await Card.Effect.Damage(3*listCount, Card);
-            return 0;
-        }
-        public async Task HandleEvent(AfterCardMove @event)
-        {
-            if (Game.GameRound.ToPlayerIndex(Game) == PlayerIndex 
-                && (Card.Status.CardRow.IsInDeck() || Card.Status.CardRow.IsInHand())
-                && @event.Target != @event.Source)
-            {
-                await Card.Effect.Boost(1, Card);
-            }
-            return;
-        }
-*/
         public Gascon(GameCard card) : base(card) { }
         public override async Task<int> CardPlayEffect(bool isSpying, bool isReveal)
         {
-            int moveCount = (Card.Status.Strength + Card.Status.HealthStatus)/2;
-            var i = 0;
+            int moveCount = (Card.Status.Strength + Card.Status.HealthStatus - 1)/2;
+            int i = 0;
             var cards = Game.GetAllCard(Card.PlayerIndex, isHasConceal: true).Where(x => x.Status.CardRow.IsOnPlace() && x != Card).ToList();
             foreach (var card in cards)
             {
@@ -56,13 +25,13 @@ namespace Cynthia.Card
                     continue;
                 }
                 await card.Effect.Move(new CardLocation(target, Game.RowToList(card.PlayerIndex, target).Count), Card);
-                i = i+1;
+                i++;
                 if (i >= moveCount)
                 {
                     break;
                 }
             }
-            await Card.Effect.Damage(2*moveCount, Card);
+            await Card.Effect.Damage(2*i, Card);
             return 0;
         }
         public async Task HandleEvent(AfterCardMove @event)
